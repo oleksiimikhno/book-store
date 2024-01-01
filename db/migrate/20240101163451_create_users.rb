89c56cda8ceaa041113fe5fdc0aff5df334bf8d8
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-01 19:34:45 +0200

    add migrations

---
+++ db/migrate/20240101163451_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
