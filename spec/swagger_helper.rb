2c321028f6b440c4940779ef436d523d9398e64c
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-05 20:31:16 +0200

    discribe product create

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,19 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'https://{defaultHost}',
+          url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'www.example.com'
+              default: '127.0.0.1:3000'
+            }
+          }
+        },
+        {
+          # TODO add external server for the API
+          url: 'http://{apiServer}',
+          variables: {
+            defaultHost: {
+              default: 'apiserver.com.ua'
             }
           }
         }
