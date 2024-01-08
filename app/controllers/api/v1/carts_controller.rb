5a895379458582d1fa9bf4792acdffdc0c27a881
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-08 14:47:29 +0200

    upd cart_controller

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,3 +1,50 @@
 class Api::V1::CartsController < ApplicationController
+  before_action :cart_params, only: %i[create update]
+  before_action :current_user
+  before_action :set_carts, only: :index
+  before_action :set_cart, only: %i[show update destroy]
 
+  def index
+    render_success(data: @carts)
+  end
+
+  def show
+    render_success(data: @cart)
+  end
+
+  def create
+    cart = @user.carts.create!(cart_params)
+
+    render_success(data: cart, status: :created)
+  end
+
+  def update
+    @cart.update!(cart_params)
+
+    render_success(data: @cart, status: :ok)
+  end
+
+  def destroy
+    @cart.destroy!
+
+    render_success(data: { message: 'Cart successfully deleted' }, status: :ok)
+  end
+
+  private
+
+  def current_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_cart
+    @cart = @user.carts.find(params[:id])
+  end
+
+  def set_carts
+    @carts = @user.carts
+  end
+
+  def cart_params
+    params.permit(:status)
+  end
 end
