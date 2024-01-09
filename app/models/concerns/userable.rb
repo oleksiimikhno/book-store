8886d0e6f2ae411cc691631e710a8e5d8ba746b5
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-09 17:53:46 +0200

    add minmax lenght passowrd

--- app/models/concerns/userable.rb
+++ app/models/concerns/userable.rb
@@ -25,6 +25,7 @@ module Userable
 
     def self.validate_password
       validates :password, presence: true,
+                           length: { in: Constable::PASSWORD_LENGTH },
                            format: {
                              with: Constable::REGEXP_PASSWORD,
                              message: 'Must contain at least one uppercase letter, one lowercase letter, and one digit'
