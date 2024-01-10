3daaf3a9583bfd956c20940f6ea4bd2f8c18cc49
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-10 22:06:13 +0200

    add authorize logic and current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 class ApplicationController < ActionController::API
-  rescue_from StandardError, with: :render_errors
   include Renderable
+  include Tokenable
+
+  before_action :authorize_request
+  rescue_from StandardError, with: :render_errors
+
+  private
+
+  def authorize_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    @decoded = decoded_token(token)
+    current_user
+  end
+
+  def current_user
+    User.find(@decoded[:user_id]) if @decoded.present?
+  end
 end
