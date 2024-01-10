b6876d9cc47a9a351c99a8dbba89aa855027a922
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-10 20:07:37 +0200

    rename passowrd to pasword_digest for JWT

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_02_174343) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_10_180350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
     t.integer "status", default: 0
+    t.integer "integer", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,7 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_02_174343) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
