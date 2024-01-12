4a3cbdc414e6151a8e5e629698cf4c809901fa66
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-13 00:44:40 +0200

    add update_password route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/login', to: 'authentications#login'
+      post '/update_password', to: 'authentications#update_password'
 
       resources :users
       resources :products
