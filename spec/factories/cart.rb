2aa31aca20384d199fb64e18d00420aa6d4aadd3
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-12 14:41:09 +0200

    upd factories

--- spec/factories/cart.rb
+++ spec/factories/cart.rb
@@ -3,16 +3,8 @@
 require 'ffaker'
 
 FactoryBot.define do
-  factory :user do
-    first_name { FFaker::Name.first_name }
-    last_name { FFaker::Name.last_name }
-    email { FFaker::Internet.free_email }
-    password { 'Password123' }
-  end
-
   factory :cart do
-    status { ',
-              user_id: { type: :integer }' }
+    status { 'unpaid' }
     association :user
   end
 end
