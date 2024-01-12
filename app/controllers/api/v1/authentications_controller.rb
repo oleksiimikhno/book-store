ba1924bed71c722cce3164bd9126648305ca164c
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-13 00:42:28 +0200

    fix auth response

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -5,13 +5,17 @@ class Api::V1::AuthenticationsController < ApplicationController
     user = User.find_by_email(user_params[:email])
 
     if user&.authenticate(params[:password])
-    token = encode_token(user_id: user.id)
-    user_data = { user: user, token: token[:token], expires: token[:expires] }
+      token = encode_token(user_id: user.id)
+      user_data = { user: user, token: token[:token], expires: token[:expires] }
 
-    render_success(data: user_data)
+      render_success(data: user_data)
     else
-      render_success(data: {errrr: 'qwe'})
+      render json: { erros: 'Wrong email or password!' }, status: :unauthorized
     end
+  end
+
+  def update_password
+
   end
 
   private
