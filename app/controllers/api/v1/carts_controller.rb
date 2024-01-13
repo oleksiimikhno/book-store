3dad0dbc487985501542999489b2a61eecdc3ecd
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-13 22:02:20 +0200

    add cart serializer

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -8,23 +8,23 @@ class Api::V1::CartsController < ApplicationController
   before_action :set_cart, only: %i[show update destroy]
 
   def index
-    render_success(data: @carts)
+    render_success(data: @carts, each_serializer: Api::V1::CartSerializer)
   end
 
   def show
-    render_success(data: @cart)
+    render_success(data: @cart, serializer: Api::V1::CartSerializer)
   end
 
   def create
     cart = @user.carts.create!(cart_params)
 
-    render_success(data: cart, status: :created)
+    render_success(data: cart, status: :created, serializer: Api::V1::CartSerializer)
   end
 
   def update
     @cart.update!(cart_params)
 
-    render_success(data: @cart, status: :ok)
+    render_success(data: @cart, status: :ok, serializer: Api::V1::CartSerializer)
   end
 
   def destroy
