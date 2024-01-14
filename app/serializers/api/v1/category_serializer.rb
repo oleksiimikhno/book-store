8472c747f2b94f1cf9d8dce5049782058cd918bf
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-14 20:50:13 +0200

    add products to category serializer

--- app/serializers/api/v1/category_serializer.rb
+++ app/serializers/api/v1/category_serializer.rb
@@ -2,4 +2,6 @@
 
 class Api::V1::CategorySerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :meta_title, :meta_description, :created_at
+
+  has_many :products, each_serializer: Api::V1::ProductSerializer
 end
