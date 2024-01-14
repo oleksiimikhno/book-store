e0786557fdcacb5c3adc1e1d30ba710b4840f597
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-14 18:30:54 +0200

    quick fix cart migration

--- db/migrate/20240101163723_create_carts.rb
+++ db/migrate/20240101163723_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[7.1]
   def change
     create_table :carts do |t|
-      t.integer :status, :integer, default: 0
+      t.integer :status, default: 0
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
