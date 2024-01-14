e51d1cf6c8e861916fce7d47e5b85d06337f8ca7
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-14 23:24:00 +0200

    upd relations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,7 @@
 class Cart < ApplicationRecord
   belongs_to :user
+  has_many :products
+  has_many :products, through: :cart_items
 
   enum status: %i[unpaid paid canceled]
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,8 @@
 class Product < ApplicationRecord
   include Productable
 
+  has_many :carts
+  has_many :carts, through: :cart_items
+
   enum :status, %i[active inactive archived], default: :active
 end
