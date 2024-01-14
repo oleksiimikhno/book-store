406de58688d556cb35315ca666e514aa6cca93fb
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-14 20:02:28 +0200

    update category migration

--- db/migrate/20240114170441_create_categories.rb
+++ db/migrate/20240114170441_create_categories.rb
@@ -2,7 +2,9 @@ class CreateCategories < ActiveRecord::Migration[7.1]
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :description
+      t.text :description
+      t.string :meta_title
+      t.text :meta_description
 
       t.timestamps
     end
