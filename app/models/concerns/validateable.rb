e162ad40eed567a0702235ba319f276972b23033
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-15 16:19:37 +0200

    add test to cart_items, upd swagger

--- app/models/concerns/validateable.rb
+++ app/models/concerns/validateable.rb
@@ -14,5 +14,32 @@ module Validateable
     def self.validate_field_positive_integer(name, lenght = 0)
       validates name, presence: true, numericality: { greater_than_or_equal_to: lenght }
     end
+
+    def self.validate_user_name(name = :first_name)
+      validates name, presence: true,
+                length: { in: Constable::NAME_LENGTH },
+                format: {
+                  with: Constable::REGEXP_USER,
+                  message: 'Only latin letters allowed, no spaces or special characters'
+                }
+    end
+
+    def self.validate_email
+      validates :email, presence: true, uniqueness: true,
+                length: { in: Constable::EMAIL_LENGTH },
+                format: {
+                  with: Constable::REGEXP_EMAIL,
+                  message: 'Should be in the format: test@test.com'
+                }
+    end
+
+    def self.validate_password
+      validates :password, presence: true,
+                length: { in: Constable::PASSWORD_LENGTH },
+                format: {
+                  with: Constable::REGEXP_PASSWORD,
+                  message: 'Must contain at least one uppercase letter, one lowercase letter, and one digit'
+                }
+    end
   end
 end
