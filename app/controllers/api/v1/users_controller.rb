3323e3128948c316b02ccb12f64b1dc27bbe3756
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-01-16 20:40:14 +0200

    fix some logic

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 class Api::V1::UsersController < ApplicationController
-  skip_before_action :authorize_request, only: %i[create destroy]
+  skip_before_action :authorize_request, only: %i[create]
   before_action :user_params, only: %i[create update]
-  before_action :set_users, only: :index
 
   def show
-    render_success(data: @user, serializer: Api::V1::UserSerializer)
+    render_success(data: current_user, serializer: Api::V1::UserSerializer)
   end
 
   def create
@@ -14,26 +13,18 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    @user.update!(user_params)
+    current_user.update!(user_params)
 
-    render_success(data: @user, status: :ok, serializer: Api::V1::UserSerializer)
+    render_success(data: current_user, status: :ok, serializer: Api::V1::UserSerializer)
   end
 
   def destroy
-    @user.destroy!
+    current_user.destroy!
 
     render_success(data: { message: 'User successfully deleted' }, status: :ok)
   end
 
   private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  def set_users
-    @users = User.all
-  end
 
   def user_params
     params.permit(:first_name, :last_name, :email, :password)
