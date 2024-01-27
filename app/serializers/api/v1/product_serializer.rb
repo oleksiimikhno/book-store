4d5feac4c301ce32b12d983aa93cf832b3630e51
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-27 20:12:16 +0200

    update serializer for product

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -2,5 +2,5 @@
 
 class Api::V1::ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :meta_title, :meta_description, :quantity, :price, :status, :category_id,
-             :created_at
+             :image, :images, :created_at
 end
