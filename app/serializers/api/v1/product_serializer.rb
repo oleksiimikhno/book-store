e893a9b46c5d0de9f987190497b11137630c413d
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-27 21:26:47 +0200

    add logic for image url

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -1,6 +1,28 @@
 # frozen_string_literal: true
 
+include Rails.application.routes.url_helpers
+
 class Api::V1::ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :meta_title, :meta_description, :quantity, :price, :status, :category_id,
              :image, :images, :created_at
+
+  def image
+    return unless object.image.attached?
+
+    image_url(object.image)
+  end
+
+  def images
+    return unless object.images.attached?
+
+    image_url(object.images)
+  end
+
+  def image_url(images)
+    if images.is_a?(ActiveStorage::Attached::Many)
+      images.map { |image| rails_blob_path(image, only_path: true) }
+    else
+      rails_blob_path(images, only_path: true)
+    end
+  end
 end
