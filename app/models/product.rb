62a8a7d9eb81ea19e4249cf9d2951d9e8ff88379
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-31 18:11:17 +0200

    fix sort order

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,11 +10,11 @@ class Product < ApplicationRecord
   has_one_attached :image, dependent: :destroy
   has_many_attached :images, dependent: :destroy
 
-  enum :status, %i[active inactive archived], default: :active
+  enum :status, %i[active inactive awaiting archived], default: :active
 
   default_scope { order(created_at: :desc) }
-  scope :order_by_date, ->(type = :desc) { reorder(created_at: type) if type.present? }
-  scope :order_by_price, ->(type = :desc) { reorder(price: type) if type.present? }
+  scope :order_by_date, ->(type) { reorder(created_at: type) if type.present? }
+  scope :order_by_price, ->(type) { reorder(price: type) if type.present? }
 
   scope :search, ->(query) { where('name || description ILIKE ?', "%#{sanitize_sql_like(query, '%')}%") }
 end
