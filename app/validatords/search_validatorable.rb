2710a80a2e5f98083180f3c26f037ba03de457a6
Oleksii Mikhno <xioflesh@gmail.com>
2024-01-31 17:40:45 +0200

    refactored validation for search

--- app/validatords/search_validatorable.rb
+++ app/validatords/search_validatorable.rb
@@ -3,13 +3,13 @@
 class SearchValidatorable
   include ActiveModel::Validations
 
-  def initialize(params)
-    params.each_pair{|k,v|
-      self.send(:"#{k}=", v) if respond_to?(:"#{k}=")
-    }
-  end
-
   attr_accessor :query
+
+  def initialize(params)
+    params.each_pair do |k,v|
+      self.send(:"#{k}=", v) if respond_to?(:"#{k}=")
+    end
+  end
 
   validates :query, presence: true, length: { in: 3..30 }
 end
