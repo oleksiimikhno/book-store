563fc983653b8d42d57af9edcb593b12cbd8edb0
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-04 17:23:07 +0200

    fix user registration email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'support@book-store-api-tc-5855f695cf77.herokuapp.com'
 
   def registration_email
-    @user = user[:user]
+    @user = params[:user]
     @url = Rails.env.development? ? 'http://localhost:3000' : params[:host]
 
     mail(to: @user.email, subject: 'Thank you for your registration!') do |format|
