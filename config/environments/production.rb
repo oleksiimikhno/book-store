0460842201a1b8f05455425a8965e90a47f724d2
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-04 19:59:35 +0200

    move pswd to credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: 'aa9c6c538007cc',
-    password: 'b1de5416a46df4',
+    password: Rails.application.mailtrap_password,
     address: 'sandbox.smtp.mailtrap.io',
     host: 'sandbox.smtp.mailtrap.io',
     port: '2525',
