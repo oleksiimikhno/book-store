b06eb3543c72f33bd6819416f23c208ac9f6d657
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-04 17:06:27 +0200

    add application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: 'support@book-store-api-tc-5855f695cf77.herokuapp.com'
+  layout 'mailer'
 end
