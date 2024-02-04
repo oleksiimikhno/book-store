ee61b918751bac770f9dd1dbfe0727e05c4b5d7b
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-04 17:08:02 +0200

    remove host from services

--- app/services/email/registration_service.rb
+++ app/services/email/registration_service.rb
@@ -3,10 +3,9 @@ class Email::RegistrationService < ApplicationServices
 
   def initialize(user)
     @user = user
-    @host = 'request.base_url'
   end
 
   def call
-    UserMailer.with(user: @user, host: @host).registration_email.deliver_later
+    UserMailer.with(user: @user).registration_email.deliver_later
   end
 end
