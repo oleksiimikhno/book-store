21cc270aef243e5454bcce0a5a6cb912b4b07693
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-02-11 19:52:58 +0200

    try to fix routes in serializer

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-include Rails.application.routes.url_helpers
+# include Rails.application.routes.url_helpers
 
 class Api::V1::ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :meta_title, :meta_description, :quantity, :price, :status, :category_id,
