6882757ee7750bc8fbe8819c34f61cc827c6b7ac
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-11 17:10:44 +0200

    add order change status

--- app/workers/email/order_changed_status_worker.rb
+++ app/workers/email/order_changed_status_worker.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-class Email::OrderCompletedWorker
+class Email::OrderChangedStatusWorker
   include Sidekiq::Worker
 
   def perform(cart_id)
     cart = Cart.find_by(id: cart_id)
 
-    OrderMailer.with(cart: cart).order_completed_email.deliver_later if cart
+    OrderMailer.with(cart: cart).order_changed_status_email.deliver_later if cart
   end
 end
