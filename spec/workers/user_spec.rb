4e6381b7225e1edb5c6187fe56b788de0636190c
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-11 14:40:28 +0200

    fix rspec mailer

--- spec/workers/user_spec.rb
+++ spec/workers/user_spec.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-require 'sidekiq/testing'
-
 RSpec.describe 'Test sending email with sidekiq', type: :request do
   let(:user) { create(:user) }
 
   it 'send email to sidekiq queue' do
     expect do
-      RegistrationEmailWorker.perform_async(user.id)
-    end.to change(RegistrationEmailWorker.jobs, :size).by(1)
+      Email::RegistrationWorker.perform_async(user.id)
+    end.to change(Email::RegistrationWorker.jobs, :size).by(1)
   end
 
   it 'should be a correct worker' do
-    expect { RegistrationEmailWorker.perform_async }.to enqueue_sidekiq_job(RegistrationEmailWorker)
+    expect { Email::RegistrationWorker.perform_async }.to enqueue_sidekiq_job(Email::RegistrationWorker)
   end
 end
