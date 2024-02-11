5c66c04f353ce35851ef6bc946025d94b80737b5
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-02-11 17:09:45 +0200

    try to fix problem in admin product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 module Admin
   class ProductsController < Admin::ApplicationController
+
+    include Rails.application.routes.url_helpers
+
     # Overwrite any of the RESTful controller actions to implement custom behavior
     # For example, you may want to send an email after a foo is updated.
     #
