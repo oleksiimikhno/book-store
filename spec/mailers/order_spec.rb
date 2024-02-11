6dcf1551c1607d9449b2a4543cdd4c4272f32198
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-11 17:42:52 +0200

    add test for order changed status

--- spec/mailers/order_spec.rb
+++ spec/mailers/order_spec.rb
@@ -2,24 +2,41 @@ require 'rails_helper'
 
 RSpec.describe OrderMailer, type: :mailer do
   let(:cart_item) { create(:cart_item) }
-  let(:cart) { cart_item.cart }
+  let(:order) { cart_item.cart }
 
   let(:email_data) do
     {
-      subject: "Thank you for your order ##{cart.id}!",
+      subject: "Thank you for your order ##{order.id}!",
       from: ['support@book-store-api-tc-5855f695cf77.herokuapp.com']
     }
   end
 
-  context 'descibe order completed' do
-    let(:email) { OrderMailer.with(order: cart).order_completed_email.deliver_now }
+  context 'describe order completed' do
+    let(:email) { OrderMailer.with(order: order).order_completed_email.deliver_now }
 
     it 'should has an user email' do
-      expect(email.to).to eq([cart.user.email])
+      expect(email.to).to eq([order.user.email])
     end
 
     it 'should be an order subject' do
       expect(email.subject).to eq(email_data[:subject])
+    end
+
+    it 'should be the sender email' do
+      expect(email.from).to eq(email_data[:from])
+    end
+  end
+
+  context 'describe order changed status' do
+    let(:subject) { "Your order ##{order.id} status changed to #{order.status}!" }
+    let(:email) { OrderMailer.with(order: order).order_changed_status_email.deliver_now }
+
+    it 'should has an user email' do
+      expect(email.to).to eq([order.user.email])
+    end
+
+    it 'should be an order subject' do
+      expect(email.subject).to eq(subject)
     end
 
     it 'should be the sender email' do
