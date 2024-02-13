c1b8363a2bedabb9818623134e6c6d20ae5ae189
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-02-13 15:02:25 +0200

    upd redis.rb initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,23 @@
-if ENV["REDISCLOUD_URL"]
-  $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+require 'sidekiq-status'
+
+Sidekiq.configure_server do |config|
+  if Rails.env.production?
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
+  else
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
+  config.server_middleware do |chain|
+    chain.add Sidekiq::Status::ServerMiddleware
+  end
+end
+
+Sidekiq.configure_client do |config|
+  if Rails.env.production?
+    config.redis = { url: ENV['REDISCLOUD_URL'] }
+  else
+    config.redis = { url: 'redis://localhost:6379/0' }
+  end
+  config.client_middleware do |chain|
+    chain.add Sidekiq::Status::ClientMiddleware
+  end
 end
