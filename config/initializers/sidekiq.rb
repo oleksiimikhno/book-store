5e666bd8c233ff8c0baa03498843a16d771a53fb
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-02-14 21:50:10 +0200

    try to add count of retry to sidekiq v.2

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,7 +2,6 @@ require 'sidekiq'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDISCLOUD_URL'] }
-  config.default_worker_options = { 'retry' => 3 }
 end
 
 Sidekiq.configure_client do |config|
