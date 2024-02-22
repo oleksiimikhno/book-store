e07031c1fd86c403a34192a694d134e391c41ad6
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-22 20:44:48 +0200

    update serializer with new fields

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-# include Rails.application.routes.url_helpers
-
 class Api::V1::ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :meta_title, :meta_description, :quantity, :price, :status, :category_id,
-             :image, :images, :created_at
+  attributes :id, :name, :description, :meta_title, :meta_description, :quantity, :price, :discount, :model,
+             :status, :category_id, :image, :images, :created_at
 
   def image
     return unless object.image.attached?
