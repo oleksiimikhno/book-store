bd61d4bbe41865efd792f4e5acd121557d1a3d69
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-24 18:48:33 +0200

    add token to email reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,17 @@
 class UserMailer < ApplicationMailer
+  include Tokenable
+
   before_action :set_user, :set_url
 
   default from: 'support@book-store-api-tc-5855f695cf77.herokuapp.com'
 
   def registration_email
     mail(to: @user.email, subject: 'Thank you for your registration!')
+  end
+
+  def reset_password_email
+    @token = user_data_with_token(@user)
+    mail(to: @user.email, subject: 'Your link for reset the password!')
   end
 
   private
