f28ab4f951edbab7c6a7fb6a39c9498242774f2e
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-24 19:00:28 +0200

    change method to find from find_by

--- app/workers/email/reset_password_worker.rb
+++ app/workers/email/reset_password_worker.rb
@@ -3,7 +3,9 @@
 class Email::ResetPasswordWorker
   include Sidekiq::Worker
 
-  def perform(user)
+  def perform(user_id)
+    user = User.find(user_id)
+
     UserMailer.with(user: user).reset_password_email.deliver_later if user
   end
 end
