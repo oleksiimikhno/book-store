6d4b85f3ba09807c6f1b996786dccf3a32ba179a
Oleksii Mikhno <xioflesh@gmail.com>
2024-02-24 18:41:04 +0200

    add test for worker

--- spec/workers/user_spec.rb
+++ spec/workers/user_spec.rb
@@ -12,4 +12,16 @@ RSpec.describe 'Test sending email with sidekiq', type: :request do
   it 'should be a correct worker' do
     expect { Email::RegistrationWorker.perform_async }.to enqueue_sidekiq_job(Email::RegistrationWorker)
   end
+
+  context 'reset password worker test' do
+    it 'send email to sidekiq queue' do
+      expect do
+        Email::ResetPasswordWorker.perform_async(user.id)
+      end.to change(Email::ResetPasswordWorker.jobs, :size).by(1)
+    end
+  
+    it 'should be a correct worker' do
+      expect { Email::ResetPasswordWorker.perform_async }.to enqueue_sidekiq_job(Email::ResetPasswordWorker)
+    end
+  end
 end
