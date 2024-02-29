be3861501c0e4ef8edc0416319055a9a9a808190
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-02-29 16:05:41 +0200

    add label and field to schema(migrations), upd seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_11_132025) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_18_130539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,8 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_132025) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "quantity", default: 0, null: false
-    t.float "price", default: 0.0, null: false
+    t.integer "quantity", default: 0
+    t.float "price", default: 0.0
     t.bigint "cart_id", null: false
     t.bigint "product_id", null: false
     t.datetime "created_at", null: false
@@ -71,6 +71,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_132025) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "fields", force: :cascade do |t|
+    t.string "value", null: false
+    t.bigint "label_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label_id"], name: "index_fields_on_label_id"
+    t.index ["product_id"], name: "index_fields_on_product_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -83,6 +99,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_132025) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "products_labels", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "label_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label_id"], name: "index_products_labels_on_label_id"
+    t.index ["product_id"], name: "index_products_labels_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -98,5 +123,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_11_132025) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "carts", "users"
+  add_foreign_key "fields", "labels"
+  add_foreign_key "fields", "products"
   add_foreign_key "products", "categories"
+  add_foreign_key "products_labels", "labels"
+  add_foreign_key "products_labels", "products"
 end
