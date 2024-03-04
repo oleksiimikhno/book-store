19449a98836f06920f8ce680c4e085faefc7fff9
Oleksii Mikhno <xioflesh@gmail.com>
2024-03-04 21:19:40 +0200

    update controller

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,24 +2,29 @@
 
 class Api::V1::FavoritesController < ApplicationController
   before_action :favorite_params, only: %i[create destroy]
+  before_action :set_favorite, only: :destroy
 
   def index
     render_success(data: current_user.products, status: :ok, each_serializer: Api::V1::ProductSerializer)
   end
 
   def create
-    current_user.favorites.create!(favorite_params)
+    current_user.favorites.create!(product_id: params[:product_id])
 
     render_success(data: { message: 'Favorite successfully added' }, status: :created)
   end
 
   def destroy
-    current_user.favorites.destroy!(favorite_params)
+    @favorite.destroy!
 
     render_success(data: { message: 'Favorite successfully deleted' }, status: :ok)
   end
 
   private
+
+  def set_favorite
+    @favorite = current_user.favorites.find_by(product_id: params[:product_id])
+  end
 
   def favorite_params
     params.permit(:product_id)
