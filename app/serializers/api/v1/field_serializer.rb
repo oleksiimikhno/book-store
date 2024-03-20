c8b66ebf9da1d84581f86db1bc7ed13856bdc293
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-03-20 21:36:08 +0200

    upd serializer for labels and fields

--- app/serializers/api/v1/field_serializer.rb
+++ app/serializers/api/v1/field_serializer.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class Api::V1::FieldSerializer < ActiveModel::Serializer
-  attributes :value, :label_id, :product_id
+  attributes :lable_name, :value, :product_id
+
+  def lable_name
+    Label.find(object.label_id).title
+  end
 end
