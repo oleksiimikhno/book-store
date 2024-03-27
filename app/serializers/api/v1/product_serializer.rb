79a8e882037dc39759a747ba6862b8735f631ace
VladFomenko <vlad.fomenko.ua@gmail.com>
2024-03-27 17:07:22 +0200

    try to do order in product relations

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -4,8 +4,12 @@ class Api::V1::ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :meta_title, :meta_description, :quantity, :price, :special_price, :model,
              :status, :category_id, :category_name, :image, :images, :created_at
 
-  has_many :labels, serializer: Api::V1::LabelSerializer
-  has_many :fields, serializer: Api::V1::FieldSerializer
+  has_many :labels, serializer: Api::V1::LabelSerializer do
+    object.labels.order(id: :asc)
+  end
+  has_many :fields, serializer: Api::V1::FieldSerializer do
+    object.fields.order(label_id: :asc)
+  end
 
   def category_name
     object.category.name
