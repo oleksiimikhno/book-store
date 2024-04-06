c4a6ef62d04434ef4982cef8aa019dad38d45429
Oleksii Mikhno <xioflesh@gmail.com>
2024-04-06 21:57:06 +0300

    move admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,7 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount Sidekiq::Web => '/sidekiq'
 
-  namespace :admin do
-    resources :users, :carts, :cart_items, :categories, :labels, :fields, :reviews, :coupons, only: %i[index show new create edit update destroy]
-
-    resources :products, only: %i[index show new create edit update destroy] do
-      member do
-        delete :image, action: :destroy_image
-        delete :images, action: :destroy_images
-      end
-    end
-
-    root to: 'users#index'
-  end
+  extend AdminRoutes
 
   namespace :api do
     namespace :v1 do
