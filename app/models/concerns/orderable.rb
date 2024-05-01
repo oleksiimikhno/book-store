664522f3019d706521f63814c63a868a136b0e2d
Oleksii Mikhno <xioflesh@gmail.com>
2024-05-01 07:24:49 +0300

    rework sort

--- app/models/concerns/orderable.rb
+++ app/models/concerns/orderable.rb
@@ -6,17 +6,13 @@ module Orderable
   included do
     default_scope { order_by_price(:asc) }
 
-    scope :order_by_date, ->(type) { reorder(created_at: type) if type.present? }
+    scope :order_by_date, ->(type) { reorder(created_at: type) }
+    scope :order_by_price, ->(type) { reorder(price: type) }
+    scope :order_by_rating, ->(type) { left_joins(:reviews).group(:id).reorder("AVG(reviews.rating) #{type}") }
 
     # TODO fix query soring with a price and a special price
     # scope :order_by_price, lambda { |type|
     #   reorder(Arel.sql("(CASE WHEN special_price > 0 THEN special_price ELSE price END) #{type}")) if type.present?
     # }
-
-    scope :order_by_price, ->(type) { reorder(price: type) if type.present? }
-
-    scope :order_by_rating, lambda { |type|
-      left_joins(:reviews).group(:id).reorder("AVG(reviews.rating) #{type}") if %i[asc desc].include?(type)
-    }
   end
 end
